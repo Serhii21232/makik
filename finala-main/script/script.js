2e5bb59c4970c3336a05908b75647511839c3b25
Serhii21232 <ldem100ffgg@gmail.com>
2025-03-12 20:22:19 +0200

    Add files via upload

---
+++ finala-main/script/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $('.carousele').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true, // Відображення крапок для навігації
+    arrows: true, // Відображення стрілок
+  });
+});
+
